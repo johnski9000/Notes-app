fd79b0d53f5da8e4626b2326c602b848cadf4d84
Janusz Wozniak <januszwozniak.bth@gmail.com>
2023-05-16 16:40:45 +0100

    new changes

--- src/Pages/Dashboard/Menu/MenuItem.js
+++ src/Pages/Dashboard/Menu/MenuItem.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 function MenuItem({props, handleClick}) {
-    console.log(props)
   return (
     <li onClick={() => handleClick(props.name)}>
     <img src={props.image} alt="upcoming" />
--- src/Pages/Dashboard/Selected/TodayTaskModal.js
+++ src/Pages/Dashboard/Selected/TodayTaskModal.js
@@ -1,12 +1,16 @@
 import React from "react";
 import styles from "../Dashboard.module.css";
 
-function TodayTaskModal({ close, setModal, deleteTask }) {
+function TodayTaskModal({ image, setModal, deleteTask }) {
   return (
     <div className={styles.today_task_modal}>
       <div className={styles.modal_title}>
         Task:
-        <img src={close} alt="" onClick={() => setModal(null)} />
+        <img src={image} alt="" onClick={() => setModal(null)} />
+      </div>
+      <div className={styles.taskData}>
+        <div className={styles.taskTitle}></div>
+        <div className={styles.taskDescription}></div>
       </div>
       <div className={styles.modal_buttons}>
         <button onClick={(e) => deleteTask(e)}>Delete Task</button>
