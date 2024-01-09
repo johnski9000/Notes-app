2de72dd65434acf9598909905c6152a33a3c0624
Janusz Wozniak <januszwozniak.bth@gmail.com>
2024-01-09 15:45:52 +0000

    sticky notes have the ability to be deleted or updated now

--- src/Pages/Dashboard/Selected/StickyNote.js
+++ src/Pages/Dashboard/Selected/StickyNote.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import image from "../media/deletered.png";
+import check from "../media/check.png";
 import axios from "axios";
 import { apiURL, apiURLLocal } from "../../../Variables/const";
 import { useAuth } from "../../../Context/AuthContext";
 
 function StickyNote({ props }) {
+  const [selected, setSelected] = useState(false);
   const auth = useAuth();
+  const contentEditableRef = useRef(null);
   const deleteNote = () => {
     console.log("Delete Note");
     axios
@@ -18,10 +21,39 @@ function StickyNote({ props }) {
         window.location.reload();
       });
   };
+  const updateNote = () => {
+    console.log("Update Note");
+    console.log(contentEditableRef.current.children[2].innerText);
+    axios.put(apiURLLocal + "/notes/update", {
+      email: auth.currentUser.email,
+      id: props.id,
+      data: {
+        title: contentEditableRef.current.children[2].innerText,
+        body: contentEditableRef.current.children[3].innerText,
+      },
+    });
+  };
 
+  const handleContentChange = () => {
+    const isSelected = document.activeElement === contentEditableRef.current;
+
+    if (isSelected) {
+      console.log("Contenteditable is selected");
+      setSelected(true);
+    } else {
+      console.log("Contenteditable is not selected");
+      setSelected(false);
+    }
+  };
   return (
     <li>
-      <a href="#" contenteditable="true" className="relative">
+      <a
+        href="#"
+        ref={contentEditableRef}
+        contenteditable="true"
+        className="relative"
+        onInput={(e) => handleContentChange(e)}
+      >
         <img
           src={image}
           alt="Delete"
@@ -30,8 +62,23 @@ function StickyNote({ props }) {
             deleteNote();
           }}
         />
-        <h2 className="pb-2 font-bold text-l">{props.title}</h2>
-        <p>{props.body}</p>
+        {selected && (
+          <img
+            src={check}
+            alt="Update"
+            className="w-8 h-8 absolute right-[5px] bottom-[10px] hover:cursor-pointer "
+            onClick={() => {
+              updateNote();
+            }}
+          />
+        )}
+        <h2
+          className="pb-2 font-bold text-l"
+          onChange={(e) => handleContentChange(e)}
+        >
+          {props.title}
+        </h2>
+        <p onChange={(e) => handleContentChange(e)}>{props.body}</p>
       </a>
     </li>
   );
